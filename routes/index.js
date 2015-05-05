84b804a372bd9fcb36fdfd47821660eb7e21dca3
Gabor Csardi <csardi.gabor@gmail.com>
2015-05-05 02:27:41 -0400

    Recently updated, good to go for alpha testing

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,19 @@ var router = express.Router();
 var get_stats = require('../lib/get_stats');
 var top_downloaded = require('../lib/top_downloaded');
 var trending = require('../lib/trending');
+var recent = require('../lib/recent');
 
 router.get('/', function(req, res) {
 
     top_downloaded(function(cb1) {
 	trending(function(cb2) {
-	    console.log(cb2.map(function(x) { return x.Package; }))
-	    res.render('index', { 'downloads': cb1,
-				  'trending': cb2
-				});
+	    recent(function(cb3) {
+
+		res.render('index', { 'downloads': cb1,
+				      'trending': cb2,
+				      'recent': cb3
+				    });
+	    })
 	})
     });
 
