fe84fb39341f28327240fb13c55fa75627eb79d7
Gabor Csardi <csardi.gabor@gmail.com>
2015-05-08 12:27:25 -0400

    Index: download stats in parallel, fixes #25

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,22 @@ var router = express.Router();
 var top_downloaded = require('../lib/top_downloaded');
 var trending = require('../lib/trending');
 var recent = require('../lib/recent');
+var async = require('async');
 
 router.get('/', function(req, res) {
 
-    top_downloaded(function(cb1) {
-	trending(function(cb2) {
-	    recent(function(cb3) {
-
-		res.render('index', { 'downloads': cb1,
-				      'trending': cb2,
-				      'recent': cb3
-				    });
-	    })
-	})
-    });
-
+    async.parallel(
+	{ 'downloads': function(cb) {
+	    top_downloaded(function(res) { cb(null, res) }) },
+	  'trending':  function(cb) {
+	    trending(function(res) { cb(null, res) }) },
+	  'recent':    function(cb) {
+	    recent(function(res) { cb(null, res) }) }
+	},
+	function(err, results) {
+	    res.render('index', results);
+	}
+    )
 });
 
 module.exports = router;
