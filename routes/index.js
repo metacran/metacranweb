6240d4436c800dfb4631bc3c00642f6351f21bea
Gabor Csardi <csardi.gabor@gmail.com>
2015-05-08 20:33:05 -0400

    Rewrite callbacks to return error
    Apparently, this is an idiom in JS.

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,20 @@ var top_downloaded = require('../lib/top_downloaded');
 var trending = require('../lib/trending');
 var recent = require('../lib/recent');
 var async = require('async');
+var handle_error = require('../lib/handle_error');
 
 router.get('/', function(req, res) {
 
     async.parallel(
 	{ 'downloads': function(cb) {
-	    top_downloaded(res, function(r) { cb(null, r) }) },
+	    top_downloaded(function(e, r) { cb(e, r) }) },
 	  'trending': function(cb) {
-	    trending(res, function(r) { cb(null, r) }) },
+	    trending(function(e, r) { cb(e, r) }) },
 	  'recent': function(cb) {
-	    recent(res, function(r) { cb(null, r) }) }
+	    recent(function(e, r) { cb(e, r) }) }
 	},
 	function(err, results) {
+	    if (err) { return handle_error(res, err); }
 	    res.render('index', results);
 	}
     )
--- routes/pkg.js
+++ routes/pkg.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var get_package = require('../lib/get_package');
 var get_revdeps = require('../lib/get_revdeps');
 var async = require('async');
+var handle_error = require('../lib/handle_error');
 
 re_full = new RegExp("^/([\\w\\.]+)$", 'i');
 
@@ -16,11 +17,12 @@ function do_query(res, package) {
     async.parallel(
 	{
 	    'pkg': function(cb) {
-		get_package(package, res, function(r) { cb(null, r)}) },
+		get_package(package, function(e, r) { cb(e, r)}) },
 	    'revdeps': function(cb) {
-		get_revdeps(package, res, function(r) { cb(null, r)}) }
+		get_revdeps(package, function(e, r) { cb(e, r)}) }
 	},
 	function(err, results) {
+	    if (err) { return handle_error(res, err) }
 	    results.pkg_link = function(x) {
 		return '<a href="/pkg/' + x + '">' + x + '</a>';
 	    };
