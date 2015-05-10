01b9753287970f448d466193ee283bceb679b675
Gabor Csardi <csardi.gabor@gmail.com>
2015-05-10 13:25:43 -0400

    Numbers on index page, fixes #7

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,25 @@ var router = express.Router();
 var top_downloaded = require('../lib/top_downloaded');
 var trending = require('../lib/trending');
 var recent = require('../lib/recent');
+var num_active = require('../lib/num_active');
+var num_maint = require('../lib/num_maint');
+var num_downloads = require('../lib/num_downloads');
+var num_updates = require('../lib/num_updates');
 var async = require('async');
 var handle_error = require('../lib/handle_error');
 
 router.get('/', function(req, res) {
 
     async.parallel(
-	{ 'downloads': function(cb) {
+	{ 'numactive': function(cb) {
+	    num_active(function(e, r) { cb(e, r) }) },
+	  'nummaint': function(cb) {
+	    num_maint(function(e, r) { cb(e, r) }) },
+	  'numupdates': function(cb) {
+	    num_updates(function(e, r) { cb(e, r) }) },
+	  'numdownloads': function(cb) {
+	    num_downloads(function(e, r) { cb(e, r) }) },
+	  'downloads': function(cb) {
 	    top_downloaded(function(e, r) { cb(e, r) }) },
 	  'trending': function(cb) {
 	    trending(function(e, r) { cb(e, r) }) },
