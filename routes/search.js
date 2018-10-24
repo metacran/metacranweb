6e5aaeb518407eb6004f709cc74a11070d312d2f
Gábor Csárdi <csardi.gabor@gmail.com>
2018-10-24 17:01:53 +0100

    Revert "Search default to OR"
    This reverts commit c4afcc8e2027edcf6a2a10df4fcf486ccf445f5f.

--- routes/search.js
+++ routes/search.js
@@ -31,6 +31,7 @@ function do_query(req, res, query) {
 			fields: ["Package^10", "Title^5", "Description^2",
 				 "Author^3", "Maintainer^4", "_all" ],
 			query: req.query['q'],
+			operator: "and",
 			"minimum_should_match": "20%"
 		    } },
 		    "functions": [
