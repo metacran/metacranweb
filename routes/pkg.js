45eff71db54935e7b003b869e953d42f909f11df
Gábor Csárdi <csardi.gabor@gmail.com>
2022-05-06 09:23:20 +0200

    Removing docs server (incomplete)

--- routes/pkg.js
+++ routes/pkg.js
@@ -4,8 +4,6 @@ var get_package = require('../lib/get_package');
 var get_revdeps = require('../lib/get_revdeps');
 var get_readme = require('../lib/get_readme');
 var get_news = require('../lib/get_news');
-var get_pkg_tvs = require('../lib/get_pkg_tvs');
-var get_gh_stars = require('../lib/get_gh_stars');
 var async = require('async');
 var pkg_link = require('../lib/pkg_link');
 var meta = require('metacran-node');
@@ -31,9 +29,7 @@ function do_query(res, next, package) {
 	    'news': function(cb) {
 		get_news(package, function(e, r) { cb(e, r)}) },
 	    'taskviews': function(cb) {
-		get_pkg_tvs(package, function(e, r) { cb(e, r)}) },
-	    'stars': function(cb) {
-		get_gh_stars(package, function(e, r) { cb(e, r)}) }
+		get_pkg_tvs(package, function(e, r) { cb(e, r)}) }
 	},
 	function(err, results) {
 	    if (err) { return next(err) }
