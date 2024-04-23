18af9d2ca3be2af6c7c2f75fd8888ea36676a8dc
Gábor Csárdi <csardi.gabor@gmail.com>
2024-04-23 13:02:07 +0200

    Fix badges

--- routes/badges.js
+++ routes/badges.js
@@ -15,8 +15,8 @@ var svg_colors = {
     "blue": "007ec6"
 };
 
-var badge_svg = `/*
-<svg xmlns="http://www.w3.org/2000/svg" width=":width:" height="20" aria-label=":text: :message:">
+var badge_svg =
+`<svg xmlns="http://www.w3.org/2000/svg" width=":width:" height="20" aria-label=":text: :message:">
   <linearGradient id="b" x2="0" y2="100%">
     <stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
     <stop offset="1" stop-opacity=".1"/>
@@ -46,95 +46,70 @@ var badge_svg = `/*
   </g>
 </svg>`
 
-router.get("/", function(req, res) {
-    res.render('underconstruction');
-})
-
 var re_pre = '^/(version|last-release|ago|version-ago|version-last-release)/';
 var re_pkg = '([\\w\\.]+)';
 var re_suf = '$';
 var re_full = new RegExp(re_pre + re_pkg + re_suf, 'i');
 
-router.get(re_full, function(req, res, next) {
-    var type = req.params[0];
-    var pkg = req.params[1];
+router.get(re_full, function (req, res, next) {
+    try {
+        var type = req.params[0];
+        var pkg = req.params[1];
 
-    var now = new Date();
-    var fivemin = new Date(now.getTime() + 5 * 60000).toUTCString();
-    res.set('Content-Type', 'image/svg+xml');
-    res.set('Expires', fivemin);
-    res.set('Cache-Control', 'max-age=300, public');
+        var now = new Date();
+        var fivemin = new Date(now.getTime() + 5 * 60000).toUTCString();
+        res.set('Content-Type', 'image/svg+xml');
+        res.set('Expires', fivemin);
+        res.set('Cache-Control', 'max-age=300, public');
 
-    if (type == 'version') {
-	return do_version_badge(res, next, pkg, req.query);
-    } else {
-	return do_lastrelease_badge(res, next, pkg, req.query, type);
+        if (type == 'version') {
+            return do_version_badge(res, next, pkg, req.query);
+        } else {
+            return do_lastrelease_badge(res, next, pkg, req.query, type);
+        }
+    } catch (err) {
+        next(err);
     }
-
 });
 
-function do_version_badge(res, next, pkg, query) {
-
-    var url = urls.crandb + '/-/desc?keys=["' + pkg + '"]';
-    request(url, function(error, response, body) {
-	if (error || response.statusCode != 200) {
-	    return next(error || response.statusCode);
-	}
-        try {
-	    var pbody = JSON.parse(body);
-	    var message = "not published";
-	    if (pbody[pkg]) {
-	        message = pbody[pkg]["version"] || "not published";
-	    }
-	    var svg = make_badge(res, "CRAN", message, query);
-
-	    res.set(200);
-	    res.send(svg);
-        } catch(err) {
-            return next(err);
-        }
-    });
+async function do_version_badge(res, next, pkg, query) {
+    // this does not fail for non-existing packages currently
+    const url = urls.crandb + '/-/desc?keys=["' + pkg + '"]';
+    const body = await ky.get(url).json();
+    const message = !!body[pkg] ? body[pkg]["version"] : "not published";
+    const svg = make_badge(res, "CRAN", message, query);
+    res.set(200);
+    res.send(svg);
 }
 
-function do_lastrelease_badge(res, next, pkg, query, type) {
+async function do_lastrelease_badge(res, next, pkg, query, type) {
+    const url = urls.crandb + '/' + pkg;
+    const resp = await ky.get(url, { throwHttpErrors: false });
+    var svg;
+    if (resp.statusCode == 404) {
+        svg = make_badge(res, 'CRAN', 'not published', query);
+    } else {
+        var json = await resp.json();
+        var d = new Date(json.date);
+        var now = new Date();
+        if (d > now) { d = now; }
+        var ver = json.Version;
+        var str = d.toISOString().slice(0, 10);
+        var ago = moment(d).fromNow();
 
-    var url = urls.crandb + '/' + pkg;
-    request(url, function(error, response, body) {
-	if (error) {
-	    return next(error || response.statusCode);
+        if (type == 'last-release') {
+            svg = make_badge(res, 'CRAN', str, query);
+        } else if (type == 'ago') {
+            svg = make_badge(res, 'CRAN', ago, query);
+        } else if (type == 'version-ago') {
+            svg = make_badge(res, 'CRAN', ver + ' – ' + ago, query);
+        } else if (type == 'version-last-release') {
+            svg = make_badge(res, 'CRAN', ver + ' – ' + str, query);
         }
+    }
 
-        try {
-	    if (response.statusCode == 404) {
-	        svg = make_badge(res, 'CRAN', 'not published', query);
-
-	    } else {
-	        var json = JSON.parse(body);
-	        var svg;
-	        var d = new Date(json.date);
-	        var now = new Date();
-	        if (d > now) { d = now; }
-	        var ver = json.Version;
-	        var str = d.toISOString().slice(0, 10);
-	        var ago = moment(d).fromNow();
-
-	        if (type == 'last-release') {
-		    svg = make_badge(res, 'CRAN', str, query);
-	        } else if (type == 'ago') {
-		    svg = make_badge(res, 'CRAN', ago, query);
-	        } else if (type == 'version-ago') {
-		    svg = make_badge(res, 'CRAN', ver + ' – ' + ago, query);
-	        } else if (type == 'version-last-release') {
-		    svg = make_badge(res, 'CRAN', ver + ' – ' + str, query);
-	        }
-            }
-
-	    res.set(200);
-	    res.send(svg);
-        } catch(err) {
-            return next(err);
-        }
-    });
+    res.set(200);
+    res.send(svg);
 }
 
 function make_badge(res, text, message, query) {
@@ -147,22 +122,22 @@ function make_badge(res, text, message, query) {
     var len = message.length;
     var no_dots = (message.match(/\./g) || []).length
     if (message == "not published") {
-	var width = 53 + 6 * len;
-	var textwidth = 47 + 3 * len;
-	var path_d = 36 + 6 * len;
+        var width = 53 + 6 * len;
+        var textwidth = 47 + 3 * len;
+        var path_d = 36 + 6 * len;
     } else {
-	var width = 61 + 6 * len - 3 * no_dots;
-	var textwidth = 51 + 3 * len - 1.5 * no_dots;
-	var path_d = 36 + 6 * len - 1.5 * no_dots;
+        var width = 61 + 6 * len - 3 * no_dots;
+        var textwidth = 51 + 3 * len - 1.5 * no_dots;
+        var path_d = 36 + 6 * len - 1.5 * no_dots;
     }
 
-    svg = badge_svg
+    const svg = badge_svg
         .replace(/:text:/g, text)
-	.replace(/:color:/g, '#' + color.replace(/[^\w]/g, ''))
-	.replace(/:width:/g, width)
-	.replace(/:textwidth:/g, textwidth)
-	.replace(/:path_d:/g, path_d)
-	.replace(/:message:/g, message)
+        .replace(/:color:/g, '#' + color.replace(/[^\w]/g, ''))
+        .replace(/:width:/g, width)
+        .replace(/:textwidth:/g, textwidth)
+        .replace(/:path_d:/g, path_d)
+        .replace(/:message:/g, message)
 
     return svg;
 }
